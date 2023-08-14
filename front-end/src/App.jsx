80b1c19a434eb9851a181332ea804e549c295287
Ana Savegnagoe <anasavegnago.castro@gmail.com>
2023-08-14 17:45:28 -0300

    primeiro commit

---
+++ front-end/src/App.jsx
@@ -0,0 +1,34 @@
+import { useState } from 'react'
+import './App.css'
+
+function App() {
+
+  return (
+    <>
+      <header>
+        <h1>Login</h1>
+      </header>
+
+    <main>
+      
+    <form action="">
+
+<div>
+  <label htmlFor="email">Email</label>
+    <input type="email" id='email' placeholder='insira seu email' />
+</div>
+
+<div>
+  <label htmlFor="">Senha</label>
+  <input type="password" id='senha' placeholder='insira sua senha'/>
+  </div>
+
+</form>
+
+    </main>
+    
+    </>
+  )
+}
+
+export default App
